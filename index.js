169f75a7d987a84394101daf8dd717baff8e6dc8
Aswin K Ashok <aswinkashok@Aswins-MacBook-Air.local>
2022-07-22 11:34:51 +0530

    env created

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const multer = require('multer')
+
+const upload = multer({dest: 'uploads/'})
+
 
 app.get('/',(req,res)=>{
 	res.send("hello world")
+})
+
+app.post('/images', upload.single('image'), (req, res) => {
+	const file = req.file
+	console.log(file)
+	res.send("image is uploading...")
 })
 
 app.listen(5000,()=> console.log("application running on port 5000"))
